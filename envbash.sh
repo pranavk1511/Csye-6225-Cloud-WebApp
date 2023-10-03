bfac1611066abac7a1e679f3a2726944827bc351
Pranav Kulkarni <37613296+pranavk1511@users.noreply.github.com>
2023-10-02 23:04:11 -0400

    Assignment 3 (#50)
    * Building Web-App
    
    * Adding Test Files
    
    * Added .yml FIles
    
    * Changed Folder Structure
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Adding Node Version
    
    * Added Dependecy Download
    
    * Updated files
    
    * Updated yml to install Mysql
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Setting Up DB
    
    * Update CI.yaml
    
    * Configure Bcrypt
    
    * Update CI.yaml
    
    * Update app.js
    
    * Updating Code
    
    * Update app.js
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Updating query
    
    * Update CI.yaml
    
    * Adding Testing Script
    
    * Update envbash.sh

--- envbash.sh
+++ envbash.sh
@@ -4,7 +4,7 @@
 echo "DB_HOST=localhost" > .env
 echo "DB_USER=root" >> .env
 echo "DB_PASSWORD=root" >> .env
-echo "DB_NAME=Assignment-3" >> .env
+echo "DB_NAME=Assignment3" >> .env
 echo "PORT=3000" >> .env
 
 # Display a message to confirm the file has been created
