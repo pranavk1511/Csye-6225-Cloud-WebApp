12913b5c274f33b261286878b6801a0fcebe0ad7
Pranav Kulkarni <37613296+pranavk1511@users.noreply.github.com>
2023-10-24 18:19:20 -0400

    Assignment 3 (#147)
    * Update CI.yaml
    
    * Setting Up DB
    
    * Update CI.yaml
    
    * Configure Bcrypt
    
    * Update CI.yaml
    
    * Update app.js
    
    * Updating Code
    
    * Update app.js
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Updating query
    
    * Update CI.yaml
    
    * Adding Testing Script
    
    * Update envbash.sh
    
    * Update CI.yaml
    
    * Adding Exit Code
    
    * Updated app logic and CI files
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * PR
    
    * PR
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * New
    
    * Update CI.yaml
    
    * Update assignment_model.js
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Commit
    
    * Update user_model.js
    
    * Update CI.yaml
    
    * Update
    
    * Update CI.yaml
    
    * Update CI
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Adding Password
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Trying password
    
    * Updating Web_app Logic
    
    * Update app.js
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * Updating Readme
    
    * adding response
    
    * Update README.md
    
    * Update CI.yaml
    
    * Update CI.yaml
    
    * CI Check
    
    * ci Check pass
    
    * commiting files
    
    * Update app.js
    
    * Update packervalidate.yaml
    
    * Update aws-ubuntu.pkr.hcl
    
    * edit
    
    * packer
    
    * Update packervalidate.yaml
    
    * Update packervalidate.yaml
    
    * Update aws-ubuntu.pkr.hcl
    
    * Update packervalidate.yaml
    
    * Update packervalidate.yaml
    
    * Update aws-ubuntu.pkr.hcl
    
    * Update packervalidate.yaml
    
    * Update packervalidate.yaml
    
    * packr file change
    
    * Create buildami.yaml
    
    * Adding Files
    
    * Update aws-ubuntu.pkr.hcl
    
    * Update buildami.yaml
    
    * update
    
    * Update buildami.yaml
    
    * Update buildami.yaml
    
    * Update buildami.yaml
    
    * installing dependecies & Updating packer
    
    * Update aws-ubuntu.pkr.hcl
    
    * updating sql command
    
    * Update aws-ubuntu.pkr.hcl
    
    * Update aws-ubuntu.pkr.hcl
    
    * Update aws-ubuntu.pkr.hcl
    
    * Adding script while setting up ami
    
    * updating setup.sh and
    
    * Update buildami.yaml
    
    * Update README.md
    
    * Update README.md
    
    * Update aws-ubuntu.pkr.hcl
    
    * Update aws-ubuntu.pkr.hcl
    
    * Update aws-ubuntu.pkr.hcl
    
    * Update README.md
    
    * Update app.js
    
    * Update aws-ubuntu.pkr.hcl
    
    * CI Check
    
    * Update ssignmentRoutes.test.js
    
    * Update ssignmentRoutes.test.js
    
    * Update aws-ubuntu.pkr.hcl
    
    * updating build packer
    
    * Update buildami.yaml
    
    * packer
    
    * valid packer
    
    * Update aws-ubuntu.pkr.hcl
    
    * Update aws-ubuntu.pkr.hcl
    
    * Update README.md
    
    * Update README.md
    
    * CI Action Fails Now
    
    * Now The CI Will Pass
    
    * Update setup.sh
    
    * deleting git
    
    * Update setup.sh
    
    * status check
    
    * packer init check
    
    * Update aws-ubuntu.pkr.hcl
    
    * Updating to build new ami
    
    * Update aws-ubuntu.pkr.hcl
    
    * chnge
    
    * new
    
    * Update README.md
    
    * Update aws-ubuntu.pkr.hcl
    
    * Packer update
    
    * Update app.js
    
    * Update app.js
    
    * Update app.js
    
    * Changing .env
    
    * Update CI.yaml
    
    * Update README.md
    
    * Update

--- setup.sh
+++ setup.sh
@@ -37,6 +37,40 @@ sudo mysql --execute="EXIT;"
 sudo apt-get remove -y git
 
 
-# Display a message
-echo "foo"
+
+
+
+# Setting up new User
+
+sudo useradd -m webappuser
+sudo groupadd webappgroup
+
+
+# Add webappuser and admin to the webappgroup
+sudo usermod -aG webappgroup webappuser
+sudo usermod -aG webappgroup admin
+
+# Set ownership and permissions for webappuser's home directory
+sudo chown -R webappuser:webappgroup /home/webappuser
+sudo chmod -R 750 /home/webappuser
+
+
+# Set ownership and permissions for the app.js file in admin's directory
+sudo chown webappuser:webappgroup /home/admin/WebApp/app.js
+sudo chmod 750 /home/admin/WebApp/app.js
+
+
+# Add webappuser to the systemd-journal group
+sudo usermod -aG systemd-journal webappuser
+
+sudo chmod 644 /home/admin/WebApp/.env
+# Create the log file and set ownership and permissions
+
+sudo touch /var/log/webapp.log
+
+sudo chown webappuser:webappgroup /var/log/webapp.log
+
+sudo chmod 644 /var/log/webapp.log
+sudo chmod 600 /home/admin/WebApp/.env
+has context menu
 
